f30ad97f0cbc05ccebb01dc2ff81b53969d348c4
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-08-17 10:27:30 +0530

    commit gh

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias mountmac 'sudo apfs-fuse -o allow_other -v 6 /dev/sda2 /mnt/macdata'
-    alias umountmac 'sudo umount /dev/sda2'
 end
+
+alias mountmac 'sudo apfs-fuse -o allow_other -v 6 /dev/sda2 /mnt/macdata'
+alias umountmac 'sudo umount /dev/sda2'
