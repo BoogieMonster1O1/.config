4757ddbb5ce9f53662555277cc0f9b9a9667ec27
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-08-18 17:50:22 +0530

    add to path

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -x PATH /home/shrishdeshpande/bin $PATH
+
 alias mountmac 'sudo apfs-fuse -o allow_other -v 6 /dev/sda2 /mnt/macdata'
 alias umountmac 'sudo umount /dev/sda2'
