06aac7d19176d3cd062d28bad491dd84ce850d5d
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-02-24 08:52:08 +0530

    return of the king

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -x PATH /home/shrishdeshpande/bin $PATH
+set -x PATH /home/shrish/.emacs.d/bin $PATH
+set -x GPG_TTY (tty)
 
 alias mountmac 'sudo apfs-fuse -o allow_other -v 6 /dev/sda2 /mnt/macdata'
 alias umountmac 'sudo umount /dev/sda2'
